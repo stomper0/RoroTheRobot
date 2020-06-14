e3a57c9efd0684de44a64a6a82e63a4fe679cfb4
fratson94 <49911479+fratson94@users.noreply.github.com>
2020-06-14 21:01:50 +0100

    Update MotorController.ino

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -4,7 +4,7 @@
  * 2019
  */
  
-#include "Adafruit_TCS34725.h"
+#include <Adafruit_TCS34725.h>
 
 #define in1 0   // motor A fwd/rev
 #define in2 1   // motor A fwd/rev
@@ -12,13 +12,14 @@
 #define enA 3   // motor A PWM signal
 #define in4 4   // motor B fwd/rev
 #define enB 5   // motor B PWM signal
-
+#define pLed 6  // "programmed" LED
 #define btn0 7  // living room
 #define btn1 8  // hall
 #define btn2 12 // kitchen
 #define btn3 13 // dining room
 
-Adafruit_TCS34725 ColorSensor;
+Adafruit_TCS34725 leftColourSensor;
+Adafruit_TCS34725 rightColourSensor;
 
 int motorSpeedA = 0;
 int motorSpeedB = 0;
@@ -75,91 +76,36 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-
+  pinMode(pLed, OUTPUT);            //"Programmed" LED pin
   pinMode(btn0, INPUT);             // Setup for the button pins
   pinMode(btn1, INPUT);
   pinMode(btn2, INPUT);
   pinMode(btn3, INPUT);
 
   Serial.begin(9600);
-
-  if (ColorSensor.begin()) {
-    Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-  }
+  //Serial.println or Serial.print
 }
 
 void loop() {
-  uint16_t r, g, b, c, colorTemp, lux;
-
-  ColorSensor.getRawData(&r, &g, &b, &c);
-  // colorTemp = ColorSensor.calculateColorTemperature(r, g, b);
-  colorTemp = ColorSensor.calculateColorTemperature_dn40(r, g, b, c);
-  lux = ColorSensor.calculateLux(r, g, b);
-
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
-//  int switchval = digitalRead(btn0) + digitalRead(btn1) + digitalRead(btn2) + digitalRead(btn3);
-//  if (switchval>0) {
-//    if (digitalRead(btn0) == HIGH) {
-//      destinationPos = 0;
-//    } else if (digitalRead(btn1) == HIGH) {
-//      destinationPos = 1;
-//    } else if (digitalRead(btn2) == HIGH) {
-//      destinationPos = 2;
-//    } else if (digitalRead(btn3) == HIGH) {
-//      destinationPos = 3;
-//    }
-//  }
+ 
+  int switchval = digitalRead(btn0) + digitalRead(btn1) + digitalRead(btn2) + digitalRead(btn3);
+  if (switchval>0) {
+    if (digitalRead(btn0) == HIGH) {
+      destinationPos = 0;
+    } else if (digitalRead(btn1) == HIGH) {
+      destinationPos = 1;
+    } else if (digitalRead(btn2) == HIGH) {
+      destinationPos = 2;
+    } else if (digitalRead(btn3) == HIGH) {
+      destinationPos = 3;
+    }
+    digitalWrite(pLed, HIGH)
+  }
 //
 //  while (currentPos != destinationPos) {
 //    
 //  }
 //  
-//  analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-//  analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
-}  
-  // X-axis used for left and right control
-//  if (xAxis < 470) {
-//    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
-//    int xMapped = map(xAxis, 470, 0, 0, 255);
-//    // Move to left - decrease left motor speed, increase right motor speed
-//    motorSpeedA = motorSpeedA - xMapped;
-//    motorSpeedB = motorSpeedB + xMapped;
-//    // Confine the range from 0 to 255
-//    if (motorSpeedA < 0) {
-//      motorSpeedA = 0;
-//    }
-//    if (motorSpeedB > 255) {
-//      motorSpeedB = 255;
-//    }
-//  }
-//  if (xAxis > 550) {
-//    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
-//    int xMapped = map(xAxis, 550, 1023, 0, 255);
-//    // Move right - decrease right motor speed, increase left motor speed
-//    motorSpeedA = motorSpeedA + xMapped;
-//    motorSpeedB = motorSpeedB - xMapped;
-//    // Confine the range from 0 to 255
-//    if (motorSpeedA > 255) {
-//      motorSpeedA = 255;
-//    }
-//    if (motorSpeedB < 0) {
-//      motorSpeedB = 0;
-//    }
-//  }
 
-// --- commented until motors tested by range, to find the lowest possible PWM range
-//  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-//  if (motorSpeedA < 70) {
-//    motorSpeedA = 0;
-//  }
-//  if (motorSpeedB < 70) {
-//    motorSpeedB = 0;
-//  }
+  
+}
